39cd2956e1fdc1ca63a5bd7f537945d9b134550d
Mikalai_Semianiuk <Mikalai_Semianiuk@epam.com>
2020-06-30 18:37:04 +0000

    Merged PR 3475: Update chatbot
    Update chatbot

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
-const path = require("path");
+﻿const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: "./src/index.js",
+    entry: "./src/scripts/index.js",
     output: {
         path: path.join(__dirname, "/dist"),
         filename: "index_bundle.js"
