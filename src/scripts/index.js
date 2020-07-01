d422d7305251615914330d24b30fa5db3327484e
Mikalai_Semianiuk <Mikalai_Semianiuk@epam.com>
2020-07-01 09:09:29 +0000

    Merged PR 3484: Fix Map error

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,7 @@
 import '../styles/styles.scss';
 import BotAvatarSVG from '../images/botAvatar.svg';
 
+const Map = require('es6-map');
 const WebChat = require('../scripts/bot.js');
 const $ = require('jquery');
 
